2de7f0f9a59c3e68dc4ebf74ba59b9794c68d24b
KKonaOG <kkonaog@weavrstudios.dev>
2026-05-03 23:49:27 -0500

    bash -> sh compliance

--- extensions/build_extensions.sh
+++ extensions/build_extensions.sh
@@ -1,21 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-mapfile -t extensions < <(git submodule -q foreach pwd | grep extensions)
 current_dir=$(pwd)
-mkdir release
+
+mkdir -p release
 rm -rf release
+mkdir release
+
 cp registry.json release/registry.json
-for i in "${extensions[@]}"
+
+git submodule -q foreach pwd | grep extensions | while IFS= read -r i
 do
-    cd $i
+    cd "$i" || exit 1
+
     touch CHANGELOG.md
     npm install
     npm run package
 
     PACKAGE=$(ls | grep .foxe)
-    # # Move it up into extensions directory
-    
-    cp $PACKAGE ../release/$PACKAGE
+
+    # Move it up into release directory
+    cp "$PACKAGE" ../release/"$PACKAGE"
 
     PACKAGE_ID="$(jq -r .publisher package.json).$(jq -r .name package.json)"
     PACKAGE_NAME=$(jq -r .name package.json)
@@ -28,36 +32,39 @@ do
 
     # Cross-platform SHA256
     if command -v sha256sum >/dev/null 2>&1; then
-    PACKAGE_SHA256=$(sha256sum "$PACKAGE" | awk '{print $1}')
+        PACKAGE_SHA256=$(sha256sum "$PACKAGE" | awk '{print $1}')
     else
-    PACKAGE_SHA256=$(shasum -a 256 "$PACKAGE" | awk '{print $1}')
+        PACKAGE_SHA256=$(shasum -a 256 "$PACKAGE" | awk '{print $1}')
     fi
 
-    jq --arg id "$(jq -r .publisher package.json).$(jq -r .name package.json)" \
-    --arg name "$(jq -r .name package.json)" \
-    --arg description "$(jq -r .description package.json)" \
-    --arg publisher "$(jq -r .publisher package.json)" \
-    --arg homepage "$(jq -r .homepage package.json)" \
-    --arg license "$(jq -r .license package.json)" \
-    --arg version "$(jq -r .version package.json)" \
-    --arg sha256 "$PACKAGE_SHA256" \
-    --arg foxe "$PACKAGE" \
-    --argjson keywords "$PACKAGE_KEYWORDS" \
-    '
-    . += [{
-    id: $id,
-    name: $name,
-    description: $description,
-    publisher: $publisher,
-    homepage: $homepage,
-    license: $license,
-    version: $version,
-    sha256sum: $sha256,
-    foxe: $foxe,
-    keywords: $keywords
-    }]
-    ' ../release/registry.json > ../release/registry.tmp && mv ../release/registry.tmp ../release/registry.json
+    jq --arg id "$PACKAGE_ID" \
+       --arg name "$PACKAGE_NAME" \
+       --arg description "$PACKAGE_DESCRIPTION" \
+       --arg publisher "$PACKAGE_PUBLISHER" \
+       --arg homepage "$PACKAGE_HOMEPAGE" \
+       --arg license "$PACKAGE_LICENSE" \
+       --arg version "$PACKAGE_VERSION" \
+       --arg sha256 "$PACKAGE_SHA256" \
+       --arg foxe "$PACKAGE" \
+       --argjson keywords "$PACKAGE_KEYWORDS" \
+       '
+       . += [{
+         id: $id,
+         name: $name,
+         description: $description,
+         publisher: $publisher,
+         homepage: $homepage,
+         license: $license,
+         version: $version,
+         sha256sum: $sha256,
+         foxe: $foxe,
+         keywords: $keywords
+       }]
+       ' ../release/registry.json > ../release/registry.tmp \
+       && mv ../release/registry.tmp ../release/registry.json
 
     git reset --hard
     git clean -fd
+
+    cd "$current_dir" || exit 1
 done
