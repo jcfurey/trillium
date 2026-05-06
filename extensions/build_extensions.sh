d85f0178dacf7efc3b319fa1ce312246b3099046
Kyler Farrar <kyler.a.farrar@usace.army.mil>
2026-05-06 11:18:20 -0500

    Add support for prebuilt  .foxe extensions

--- extensions/build_extensions.sh
+++ extensions/build_extensions.sh
@@ -5,6 +5,60 @@ mkdir release
 
 cp registry.json release/registry.json
 
+# Extensions that are .foxe files in the extensions directory
+mapfile -d $'\0' foxe_paths < <(find $(pwd) -name "*.foxe" -print0)
+for foxe_file in ${foxe_paths[@]}; do
+    cp ${foxe_file} release/.
+    unzip ${foxe_file} -d unpacked_extension
+    cd unpacked_extension
+
+    PACKAGE_ID="$(jq -r .publisher package.json).$(jq -r .name package.json)"
+    PACKAGE_NAME=$(jq -r .name package.json)
+    PACKAGE_DESCRIPTION=$(jq -r .description package.json)
+    PACKAGE_PUBLISHER=$(jq -r .publisher package.json)
+    PACKAGE_HOMEPAGE=$(jq -r .homepage package.json)
+    PACKAGE_LICENSE=$(jq -r .license package.json)
+    PACKAGE_VERSION=$(jq -r .version package.json)
+    PACKAGE_KEYWORDS=$(jq .keywords package.json)
+
+    # Cross-platform SHA256
+    if command -v sha256sum >/dev/null 2>&1; then
+        PACKAGE_SHA256=$(sha256sum ${foxe_file} | awk '{print $1}')
+    else
+        PACKAGE_SHA256=$(shasum -a 256 ${foxe_file} | awk '{print $1}')
+    fi
+
+    jq --arg id "$PACKAGE_ID" \
+       --arg name "$PACKAGE_NAME" \
+       --arg description "$PACKAGE_DESCRIPTION" \
+       --arg publisher "$PACKAGE_PUBLISHER" \
+       --arg homepage "$PACKAGE_HOMEPAGE" \
+       --arg license "$PACKAGE_LICENSE" \
+       --arg version "$PACKAGE_VERSION" \
+       --arg sha256 "$PACKAGE_SHA256" \
+       --arg foxe "extensions/$(basename ${foxe_file})" \
+       --argjson keywords "$PACKAGE_KEYWORDS" \
+       '
+       . += [{
+         id: $id,
+         name: $name,
+         description: $description,
+         publisher: $publisher,
+         homepage: $homepage,
+         license: $license,
+         version: $version,
+         sha256sum: $sha256,
+         foxe: $foxe,
+         keywords: $keywords
+       }]
+       ' ${extensions_dir}/release/registry.json > ${extensions_dir}/release/registry.tmp \
+       && mv ${extensions_dir}/release/registry.tmp ${extensions_dir}/release/registry.json
+    cd ..
+    rm -rf unpacked_extension
+done;
+
+
+# Extensions that are submodules and need to be built
 git submodule -q foreach pwd | grep extensions | while IFS= read -r i
 do
     cd "$i" || exit 1
